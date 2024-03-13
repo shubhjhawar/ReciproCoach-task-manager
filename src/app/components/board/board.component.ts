dddaf9ca1f0e49240f5fa401fce2ede0869bff20
Shubh <shubhjhawar78@gmail.com>
2024-03-12 23:19:43 -0400

    implemented saving drag drop in localstorage

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -29,14 +29,59 @@ export class BoardComponent {
 
   drop(event: CdkDragDrop<Task[]>) {
     if (event.previousContainer === event.container) {
+      // drag drop in same column
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      // Update localStorage for same column
+
+      // getting localstorage and columnName
+      const boardFromLocalStorage = JSON.parse(localStorage.getItem("board") || "{}");
+      const columnName = event.container.element.nativeElement.getAttribute('data-column-name');
+      
+      if (columnName) {
+        // finding the column to update
+        const columnToUpdate = boardFromLocalStorage.columns.find((col: Column) => col.name === columnName);
+        if (columnToUpdate) {
+          // Update tasks array in localStorage with the new order
+          columnToUpdate.tasks = event.container.data; 
+        } else {
+          console.error("Column not found in localStorage:", columnName);
+        }
+      } else {
+        console.error("Column name not found in data attribute.");
+      }
     } else {
+      // Transfer task from previous column to current column
+      const previousColumnName = event.previousContainer.element.nativeElement.getAttribute('data-column-name');
+
+      // drag drop in different column
+      const taskToMove = event.previousContainer.data[event.previousIndex];
       transferArrayItem(
         event.previousContainer.data,
-        event.container.data,
+        event.container.data, 
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
+  
+      // getting localstorage and columnName
+      const boardFromLocalStorage = JSON.parse(localStorage.getItem("board") || "{}");
+      const columnName = event.container.element.nativeElement.getAttribute('data-column-name');
+
+      if (previousColumnName && columnName) {
+        // finding the two columns
+        const prevColToUpdate = boardFromLocalStorage.columns.find((col: Column) => col.name === previousColumnName);
+        const columnToUpdate = boardFromLocalStorage.columns.find((col: Column) => col.name === columnName);
+        if (prevColToUpdate && columnToUpdate) {
+          // updating the two columns
+          prevColToUpdate.tasks = event.previousContainer.data
+          columnToUpdate.tasks = event.container.data;
+          localStorage.setItem("board", JSON.stringify(boardFromLocalStorage));
+        } else {
+          console.error("Column not found in localStorage:", columnName);
+        }
+      } else {
+        console.error("Column name not found in data attribute.");
+      }
+
     }
   }
 
